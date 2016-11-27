76b5bbf8ca143118b71046d1b28a0aed619e66b6
Ashutosh Sandhal <ashutosh@Ashutoshs-MacBook-Air.local>
2016-11-27 11:25:47 +0530

    new testcase added for discount

---
+++ src/test/java/customerTest/verifyDiscountOrderTest.java
@@ -0,0 +1,52 @@
+package customerTest;
+
+import baseClassPackage.BaseClasses;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utilPackages.PropertyValExtractors;
+import utilPackages.WaitClass;
+import customer.DiscountPage;
+
+
+public class verifyDiscountOrderTest {
+	DiscountPage discountPage;
+    WebDriver driver,driver2;
+
+    @BeforeClass(alwaysRun=true)
+    public void setup(){
+        driver= BaseClasses.setup( "User1");
+        PropertyValExtractors p=new PropertyValExtractors();
+        p.getPropertyFile("test", "configuration.properties");
+        String url=p.getVal("url1");
+        driver.get(url);
+        WaitClass.waitForUrl(driver, url, 10);
+        discountPage=discountPage.getPage(driver, DiscountPage.class);
+
+    }
+
+    @Test(groups={"Sanity"},description="Login",priority = 1)
+    public void userSignin() throws IOException{
+    	discountPage.enterLoginID();
+    	discountPage.enterPassword();
+    	discountPage.selectCompany();
+    	discountPage.clickLoginButton();
+    	discountPage.clickOrdersTab();
+    	discountPage.clickOrder();
+    	discountPage.verifyAmountDifference();
+
+    }
+
+}
