144cd6b5b9b1a21700c5c2fa3efcc3c40dbe35fc
rinkykumari <rinkykumari@webdataconsulting.in>
2018-10-22 16:28:33 +0530

    Added Sleep to click on the + button to add dependency fields and Format the test classes

--- src/main/java/productDependency/CreateDependentCategoryPage.java
+++ src/main/java/productDependency/CreateDependentCategoryPage.java
@@ -322,9 +322,10 @@ public class CreateDependentCategoryPage extends BasePage {
 	 */
 	public void addDependencyLine() throws IOException {
 		CreateDependentCategoryPage sp = new CreateDependentCategoryPage(driver);
-		log.info("click on dependency plus icon to add the product.");
-		Assert.assertTrue(addDependencyLine.isDisplayed());
 		JavaScriptExec.scrollToElementOnPage(driver, addDependencyLine);
+		log.info("click on dependency plus icon to add the product.");
+		JavaScriptExec.sleep();
+		Assert.assertTrue(addDependencyLine.isDisplayed());
 		addDependencyLine.click();
 	}
 
